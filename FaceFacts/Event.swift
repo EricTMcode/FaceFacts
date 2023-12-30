1e8374bcabe4cd3bb3954a010244695fcc98fbce
Eric <ebagnaro@gmail.com>
2023-12-30 15:59:29 +0100

    - Add filtering

---
+++ FaceFacts/Event.swift
@@ -0,0 +1,20 @@
+//
+//  Event.swift
+//  FaceFacts
+//
+//  Created by Eric on 30/12/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Event {
+    var name: String
+    var location: String
+    
+    init(name: String, location: String) {
+        self.name = name
+        self.location = location
+    }
+}
