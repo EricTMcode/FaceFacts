04e57e0945a501ac686fc795594689b5075abb25
Eric <ebagnaro@gmail.com>
2023-12-30 15:48:05 +0100

    - Add searchable for name, emailAddress and details

--- FaceFacts/ContentView.swift
+++ FaceFacts/ContentView.swift
@@ -9,23 +9,29 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
     @State private var path = [Person]()
-    @Query var people: [Person]
+    
+    @State private var searchText = ""
     
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(people) { person in
-                    NavigationLink(value: person) {
-                        Text(person.name)
-                    }
+            PeopleView(searchString: searchText)
+                .navigationTitle("FaceFacts")
+                .navigationDestination(for: Person.self) { person in
+                    EditPersonView(person: person)
                 }
-            }
-            .navigationTitle("FaceFacts")
-            .navigationDestination(for: Person.self) { person in
-                Text(person.name)
-            }
+                .toolbar {
+                    Button("Add Person", systemImage: "plus", action: addPerson)
+                }
+                .searchable(text: $searchText)
         }
+    }
+    
+    func addPerson() {
+        let person = Person(name: "", emailAddress: "", details: "")
+        modelContext.insert(person)
+        path.append(person)
     }
 }
 
