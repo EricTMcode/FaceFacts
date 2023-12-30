552e9c49f7fcee9e31492dcdcfade9cf10f5f5be
Eric <ebagnaro@gmail.com>
2023-12-30 14:32:18 +0100

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Eric on 30/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
